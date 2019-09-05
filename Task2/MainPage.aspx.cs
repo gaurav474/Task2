e7a732faf74aa648a8ee5983bd4baf7c1a86b402
Gaurav Gajjar <Gajjarg135@outlook.com>
2019-09-05 11:56:36 +0530

    Add project files.

---
+++ Task2/MainPage.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Task2
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["strconn"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Server.MapPath("Image"))) ;
+            {
+                Directory.CreateDirectory(Server.MapPath("Image"));
+            }
+            string imgurl = (Server.MapPath("Image")) + "\\" + flImage.FileName;
+            string dbpath = "~/Image/" + flImage.FileName;
+            flImage.SaveAs(imgurl);
+            SqlCommand cmd = new SqlCommand("insert into product(ProductImage) " +
+                "values(@pimg)", conn);
+            cmd.Parameters.AddWithValue("@pimg", dbpath);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ Task2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Task2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2e91e9d-ac3a-4694-a0e8-01bdd8b5870d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
